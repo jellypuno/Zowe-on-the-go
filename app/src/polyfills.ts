ff754700d5c2d13139d1804d47d40a8f039406f1
MarkAckert <mark.ackert@broadcom.com>
2020-03-03 15:32:46 -0500

    add EPL licensing
    Signed-off-by: MarkAckert <mark.ackert@broadcom.com>

--- app/src/polyfills.ts
+++ app/src/polyfills.ts
@@ -1,3 +1,14 @@
+/*
+* This program and the accompanying materials are made available under the terms of the
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at
+* https://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Copyright Contributors to the Zowe Project.
+*
+*/
+
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
